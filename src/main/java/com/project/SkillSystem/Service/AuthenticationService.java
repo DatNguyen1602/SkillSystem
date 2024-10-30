6de3609c1b48f33349ce32c3e15e7e2ee1ed62ee
Admin <tn194972@gmail.com>
2024-10-30 14:12:58 +0700

    git push all origin main

--- src/main/java/com/project/SkillSystem/Service/AuthenticationService.java
+++ src/main/java/com/project/SkillSystem/Service/AuthenticationService.java
@@ -1,5 +1,8 @@
 package com.project.SkillSystem.Service;
 
+import com.nimbusds.jose.*;
+import com.nimbusds.jose.crypto.MACSigner;
+import com.nimbusds.jwt.JWTClaimsSet;
 import com.project.SkillSystem.Dto.Request.AuthenticationRequest;
 import com.project.SkillSystem.Dto.Response.AuthenticationResponse;
 import com.project.SkillSystem.Exception.AppException;
@@ -8,18 +11,67 @@ import com.project.SkillSystem.Repository.UserRepository;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
+import lombok.experimental.NonFinal;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
+import java.util.Date;
+
+@Slf4j
 @Service
 @RequiredArgsConstructor
 @FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
 public class AuthenticationService {
     UserRepository userRepository;
 
+    @NonFinal
+    @Value("$(jwt.signerKey")
+    protected String SIGNER_KEY;
+
     public AuthenticationResponse authenticate(AuthenticationRequest authenticationRequest) {
         var user = userRepository.findbyLdap(authenticationRequest.getLdap())
                 .orElseThrow(() -> new AppException(ErrorCode.USER_NOT_EXISTED));
 
-        return AuthenticationResponse.builder().build();
+        PasswordEncoder passwordEncoder = new BCryptPasswordEncoder(10);
+        boolean authenticated = passwordEncoder.matches(authenticationRequest.getPassword(), user.getPassword());
+
+        if (!authenticated)
+            throw new AppException(ErrorCode.UNAUTHENTICATED);
+
+        var token = generateToken(authenticationRequest.getLdap());
+
+        return AuthenticationResponse.builder()
+                .token(token)
+                .authenticated(true)
+                .build();
+    }
+
+    private String generateToken(String ldap) {
+        JWSHeader header = new JWSHeader(JWSAlgorithm.HS512);
+
+        JWTClaimsSet JWTclaimsSet = new JWTClaimsSet.Builder()
+                .subject(ldap)
+                .issuer(ldap)
+                .issueTime(new Date(
+                        Instant.now().plus(1, ChronoUnit.HOURS).toEpochMilli()
+                ))
+                .build();
+
+        Payload payload = new Payload(JWTclaimsSet.toJSONObject());
+
+        JWSObject jwsObject = new JWSObject(header, payload);
+
+        try {
+            jwsObject.sign(new MACSigner(SIGNER_KEY.getBytes()));
+            return jwsObject.serialize();
+        } catch (JOSEException e) {
+            log.error("cannot create token", e);
+            throw new RuntimeException(e);
+        }
     }
 }
